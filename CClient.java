2f9bf9d4a4a52bd3f3ad9692ebe7547b3c18464d
eslamohammed <93443985+eslamohammed@users.noreply.github.com>
2022-03-11 17:18:43 +0200

    Add files via upload

---
+++ CClient.java
@@ -0,0 +1,46 @@
+package edu.fee;
+
+import java.io.*;
+import java.net.Socket;
+
+public class SimpleClient {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client started");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Socket socket = new Socket("localhost",5050);
+
+
+        InputStream inputStream = socket.getInputStream();
+        DataInputStream socketDIS = new DataInputStream(inputStream);
+        String serverMsg = socketDIS.readUTF();
+        System.out.println(serverMsg);
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        OutputStream outputStream = socket.getOutputStream();
+        DataOutputStream socketDOS = new DataOutputStream(outputStream);
+
+        Scanner m = new Scanner(System.in);
+        String clientMsg = m.nextLine();
+
+        socketDOS.writeUTF(clientMsg);
+        System.out.println(clientMsg);
+
+
+        socketDOS.close();
+        socketDIS.close();
+        outputStream.close();
+        inputStream.close();
+        socket.close();
+    }
+}
